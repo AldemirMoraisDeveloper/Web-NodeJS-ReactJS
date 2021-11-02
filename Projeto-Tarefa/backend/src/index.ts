f4c27b6f028d3b7392a9985d871b41919293fe9c
Aldemir Ferreira <aldemirmorais29@gmail.com>
2021-11-02 18:21:30 -0300

    Removi o ormconfig.json

--- Projeto-Tarefa/backend/src/index.ts
+++ Projeto-Tarefa/backend/src/index.ts
@@ -4,9 +4,34 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as cors from 'cors';
 import routes from "./routes";
- 
+require("dotenv").config();
+
 const app = express()
-createConnection()
+const config: any = {
+    "type": "postgres",
+    "host": "tai.db.elephantsql.com",
+    "port": 5432,
+    "username": "whbaqxnp",
+    "password": "bOapbfsblCUL-FjrhuD9iWgEh2oYWCAC",
+    "database": "whbaqxnp",
+    "synchronize": true,
+    "logging": false,
+    "entities": [
+       "src/entity/**/*.ts"
+    ],
+    "migrations": [
+       "src/migration/**/*.ts"
+    ],
+    "subscribers": [
+       "src/subscriber/**/*.ts"
+    ],
+    "cli": {
+       "entitiesDir": "src/entity",
+       "migrationsDir": "src/migration",
+       "subscribersDir": "src/subscriber"
+    }
+ } 
+createConnection(config)
  
 app.use(cors())
 app.use(bodyParser.json())
